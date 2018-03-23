55eb00437a0e37e0f96cd7d9d1bd4f5cbca4e301
gonçalo <gonalo@DESKTOP-5RVGG75>
2018-03-23 20:08:20 +0000

    criação do modelo que representa as tabelas de base de dados

---
+++ Multas/Multas/Models/Agentes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Agentes
+    {
+        //criar o construtor
+        public Agentes() {
+            ListaMultas = new HashSet<Multas>();
+        }
+
+        public int ID { get; set; } //chave primaria
+
+        //dados do Agente
+        public string Nome { get; set; }
+
+        public string Fotografia { get; set; }
+
+        //local de trabalho do Agente
+        public string Esquadra { get; set; }
+
+        //Criar uma lista de multas
+        //aplicadas pelo Agente
+        // virtual - dados so sao carreados na memoria se necessario
+        public virtual ICollection<Multas> ListaMultas { get; set; }
+
+    }
+}
---
+++ Multas/Multas/Models/Viaturas.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multas.Models
+{
+    public class Viaturas
+    {
+
+        //criar o construtor
+        public Viaturas()
+        {
+            ListaMultas = new HashSet<Multas>();
+        }
+
+        public int ID { get; set; }
+
+
+        //descrição da viatura
+        public string Matricula { get; set; }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Cor { get; set; }
+
+        //descrição do dono da viatura
+        public string NomeDono { get; set; }
+
+        public string MoradaDono { get; set; }
+
+        public string CodPostalDono { get; set; }
+
+        //Criar uma lista de multas
+        //aplicadas à viatura
+        // virtual - dados so sao carreados na memoria se necessario
+        public virtual ICollection<Multas> ListaMultas { get; set; }
+    }
+}
